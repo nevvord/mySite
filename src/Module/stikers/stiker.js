8aab5d106958128422bbc1b47e8edfff8fdb2cce
Nevvord <nevvord@gmail.com>
2019-06-23 22:24:24 +0300

    Revert "Revert "Wrong""
    This reverts commit 0fda4f9583a41313fb477e7b8235b9da34e01e4f.

--- src/Module/stikers/stiker.js
+++ src/Module/stikers/stiker.js
@@ -6,12 +6,16 @@ class Sticker extends React.Component {
         works: []
     }
 
-    GetImport() {
+    data() {
+        const pop = this.props.getMethod();
+        console.log(pop);
         
-            return <h1>kek</h1>;
+            
         
     }
+
     render() {
+        this.data();
         return (
             <div className="sticker" > 
 
